9a918cef3acb700663bc8a848c3e07e4123fb1fe
Fernando Hickman <fernanhick@gmail.com>
2025-12-08 18:33:18 +0000

    latest catch up course

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -1,43 +1,80 @@
 'use strict';
 
 /// selecting elements
-const player0El = document.querySelector('.player--0')
-const player1El = document.querySelector('.player--1')
-const name0El = document.getElementById('name--0')
-const name1El = document.getElementById('name--1')
-const score0El = document.getElementById('score--0')
-const score1El = document.getElementById('score--1')
-const current0El = document.getElementById('current--0')
-const current1El = document.getElementById('current--1')
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
+const name0El = document.getElementById('name--0');
+const name1El = document.getElementById('name--1');
+const score0El = document.getElementById('score--0');
+const score1El = document.getElementById('score--1');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
 
-const btnNew = document.querySelector('.btn--new')
-const btnRoll = document.querySelector('.btn--roll')
-const btnHold = document.querySelector('.btn--hold')
-const diceEl = document.querySelector('.dice')
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+const diceEl = document.querySelector('.dice');
 
-const scores = [0, 0]
-let dice = 0
-let currentScore = 0
-let activePlayer = 0
+score0El.textContent = 0;
+score1El.textContent = 0;
 
-btnRoll.addEventListener('click', function(){
+const scores = [0, 0];
+let dice = 0;
+let currentScore = 0;
+let activePlayer = 0;
+let playing = true;
 
-    dice = Math.trunc(Math.random() * 6 ) +1
-    console.log(dice)
-    diceEl.src = `dice-${dice}.png`
-        
+const switchPlayer = function () {
+  currentScore = 0;
+  document.getElementById(`current--${activePlayer}`).textContent =
+    currentScore;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  player0El.classList.toggle('player--active');
+  player1El.classList.toggle('player--active');
+};
+
+btnRoll.addEventListener('click', function () {
+  if (playing) {
+    dice = Math.trunc(Math.random() * 6) + 1;
+    console.log(dice);
+    diceEl.src = `dice-${dice}.png`;
+
     //Check for rolled 1
-    if(dice !== 1){
-
-        //add roled number to current score
-        currentScore +=dice
-        document.getElementById(`current--${activePlayer}`).textContent = currentScore
-    }else{
-        //change user if rolled 1
-        currentScore = 0
-        document.getElementById(`current--${activePlayer}`).textContent = currentScore
-        activePlayer = activePlayer === 0 ? 1 : 0
-        player0El.classList.toggle('player--active')
-        player1El.classList.toggle('player--active')
+    if (dice !== 1) {
+      //add roled number to current score
+      currentScore += dice;
+      document.getElementById(`current--${activePlayer}`).textContent =
+        currentScore;
+    } else {
+      //change user if rolled 1
+      switchPlayer();
     }
-})
+  }
+});
+
+btnHold.addEventListener('click', function () {
+  if (playing) {
+    //1. Add current value to the  score
+    console.log(activePlayer);
+    scores[activePlayer] += currentScore;
+    document.getElementById(`score--${activePlayer}`).textContent =
+      scores[activePlayer];
+    //2. Check if player's score is 100
+    //Finish game
+    if (scores[activePlayer] >= 20) {
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.add('player--winner');
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.remove('player--active');
+
+      console.log(`Player ${activePlayer + 1} is the winner`);
+      playing = false;
+      btnRoll.removeEventListener('click');
+    } else {
+      //Switch to the next player
+      switchPlayer();
+    }
+  }
+});
